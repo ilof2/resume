a2aa560eddbd85f057dc5c9a530ddf2fbf376767
ilof2 <ilof@tuta.io>
2019-11-20 23:37:23 +0100

    resume page added

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, static_url_path=os.path.abspath("static"))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="80")
